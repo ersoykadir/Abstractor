66cddfa709988503642c814c7d8bbdf0870614bd
ersoykadir <eersooy61@gmail.com>
2022-01-13 23:18:01 +0300

    commented and cleaned

--- src/abstractor.cpp
+++ src/abstractor.cpp
@@ -17,62 +17,66 @@
 #include <semaphore.h>
 using namespace std;
 
-//double similarity(vector<string> query_set, unordered_map<vector<string>*,int> *abstract_set);
-void tokenize(string inputStr, vector<string>* tokens, char tokenSeperator);
-//void getSentences(string abstract, unordered_map<vector<string>*, int>* abstract_set);
 
-//int similarity(set<string> query_set, set<string> abstract_set);
-int similarity(set<string> query_set, set<string> abstract_set, set<string> *intersection_set);
-//double similarity(set<string> query_set, set<string> abstract_set);
-void tokenize(string inputStr, set<string>* tokens, char tokenSeperator);
-void getSentences(string abstract, unordered_map<set<string>*, int>* abstract_set);
 
+/**
+ * @brief mutex to protect the access to file_index variable, each thread gets a file from the file list with the current index and increases it by one.
+ */
 pthread_mutex_t file_index_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t output_mutex = PTHREAD_MUTEX_INITIALIZER;
+/**
+ * @brief mutex to protect write back of calculated results to logs.
+ */
 pthread_mutex_t file_list_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t wait_mutex = PTHREAD_MUTEX_INITIALIZER;
 
+/**
+ * @brief struct that holds the result info.
+ */
 struct log {
-	// have a list of logs, each thread will modify 
-	//	the appropriate log in the list at the end of calculation
-	// use mutex to modify the logs
-		double similarity=0;
+		//	Name of the abstract file.
 		string abstract_name=" ";
+		//	Jaccard similarity of this file with current query.
+		double similarity=0;
+		//	The sentence which got the most number of matching words with query.
 		string summary=" ";
 };
+
+/**
+ * @brief struct that holds the input data to threads.
+ */
 struct input_struct {
-    //string query;
+    //	The file that thread will process.
 	string input_file_name= " ";
+	//	The query of words to search in the file.
 	string query = " ";
+	//	The ID of the thread
 	int threadID = -1;
+	//	The sentence which got the most number of matching words with query for the current file that the thread is processing.
 	string summary = " ";
+	//	Jaccard similarity of the file ,which the thread is currently processing, with current query.
 	double jaccard=0;
+	//	The index of the file which the thread is currently processing.
 	int current_file_index=-1;
-	bool work = true;
+	//	Indicates whether the thread is working or not.
+	bool working = true;
 };
 
-
-// struct input_struct {
-// 	struct output_struct {
-// 		double similarity=0;
-// 		string abstract_name="";
-// 		string summary="";
-// 	};
-//     //string input_file_name;
-// 	string output_file_name;
-//     string query;
-// 	vector<string> *file_names;
-// 	int threadID;
-// 	char threadName = 'A';
-// 	output_struct output ;
-// };
-
+//	Index of file name list.
 int file_name_index=0;
+//	The number of processes that wait on mutex lock.
 int waiting_processes=0;
+//	The list of output data log.
 vector<log*> logs;
+//	The output file name that the results will be written. 
 string outfile_name;
 
-void jaccard(input_struct *inputArgs);
+/**
+ * @brief Compares two log struct by their jaccard similarity scores.
+ * 
+ * @param a log struct to be compared.
+ * @param b log struct to be compared.
+ * @return true If similarity score of a is higher than b.
+ * @return false otherwise.
+ */
 bool compareByLength(const log *a, const log *b)
 {
     return a->similarity > b->similarity;
@@ -90,10 +94,40 @@ bool compareByLength(const log *a, const log *b)
  * 2)	call join for thread_0 and || create thread_0 with next abstract || call join for next thread
  */
 
-void *runner(void*); /* threads call this function */
+/**
+ * @brief Tokenizes the given string with given seperator and fills it to the given list.
+ * 
+ * @param inputStr 			String to be tokenized.
+ * @param tokens 			Gets filled by the tokens.
+ * @param tokenSeperator 	Seperator character to tokenize the string.
+ */
+void tokenize(string inputStr, vector<string>* tokens, char tokenSeperator);
 
-int main(int argc, char* argv[]) {
+/**
+ * @brief Returns the number of intersecting words between two sets.
+ * 
+ * @param query_set 		Set of query words to look for.
+ * @param abstract_set 		Set of words to search.
+ * @param intersection_set 	Intersecting set of words in query and the file
+ * @return int 				Number of intersecting words.
+ */
+int similarity(set<string> query_set, set<string> sentence_set, set<string> *intersection_set);
 
+/**
+ * @brief Calculating the jaccard similarity score of the input.
+ * 
+ * @param inputArgs Struct that holds necessary input for the thread.
+ */
+void jaccard(input_struct *inputArgs);
+
+/**
+ * @brief Runner function that threads execute.
+ */
+void *runner(void*);
+
+
+int main(int argc, char* argv[]) 
+{
 	string infile_name = argv[1];//READ INPUT FILE NAME
 	outfile_name = argv[2];//READ OUTPUT FILE NAME
 
@@ -101,34 +135,20 @@ int main(int argc, char* argv[]) {
 	ifstream infile;
 	infile.open(infile_name);
 	ofstream outfile;
+	//	Clear the output file before appending...
 	outfile.open(outfile_name);
 	outfile.close();
+	//	...
 	outfile.open(outfile_name, std::ios::out | std::ios::app );
-	//cout << outfile_name << endl; 
-	// //SEMAPHORE
-	// sem_t sem;
-	// if(sem_init(&sem,0,1) == -1)
-	// 	printf("%s\n",strerror(errno));
-	
-	// /*	sem_wait decrements the semaphore value. Returns zero on success and -1 on error*/
-	// if(sem_wait(&sem) != 0)
-	// 	printf("%s\n",strerror(errno));
-	
-	// //Critical section ....
-	// /*	sem_post increments the semaphore. */
-	// if(sem_post(&sem) != 0)
-	// 	printf("%s\n",strerror(errno));
-	// /*	sem_destroy destroys the semaphore. */
-	// if(sem_destroy(&sem) != 0)
-	// 	printf("%s\n",strerror(errno));
 
-	vector<string> lines;
-	//vector<string> file_names;
+	vector<string> lines;//lines of the file being read
 	string line;
 	while(getline(infile, line)){
 		lines.push_back(line);
 	}
+	/*	First line holds the information about number of threads, number of files to read and number of results to return. */
     string info = lines[0];
+	//---	Reading the numbers into integers.
     stringstream ss(info);
     string thread_numStr;
     string num_abstr_to_scanStr;
@@ -140,26 +160,25 @@ int main(int argc, char* argv[]) {
         cout << "Parameters missing!!!" << endl;
         return(1);
    	}
-	int thread_num= stoi(thread_numStr);
-	int num_abstr_to_scan = stoi(num_abstr_to_scanStr);
-	int num_abstr_to_return = stoi(num_abstr_to_returnStr);
-   	string query = lines[1];
+	int thread_num= stoi(thread_numStr);//						number of threads.
+	int num_abstr_to_scan = stoi(num_abstr_to_scanStr);//		number of abstract files.
+	int num_abstr_to_return = stoi(num_abstr_to_returnStr);//	number of results to return.
+	//---
+   	string query = lines[1];//	Query words to search within files.
+	   
+	//Each file has its own log including summary and jaccard similarity score.
+	//Initializing logs.
    	for (int i = 2; i < lines.size(); ++i)
    	{
 		logs.push_back(new log);
-		logs[i-2]->abstract_name = lines[i];//!!!!!!!!!
-   		//file_names.push_back(lines[i]);
-   	}  
-	   cout << query << endl; 
-	// Disable line buffering in stdout so that output gets displayed immediately
-    //setvbuf(stdout, NULL, _IONBF, 0);
-   	// pthread_t tid1,tid2; /* thread identifiers */
-    // pthread_attr_t attr; /* set of thread attributes */
-   	/* create threads */
+		logs[i-2]->abstract_name = lines[i];
+   	}
+
 	vector<pthread_t> threads;
-	waiting_processes = thread_num;
-	cout << query << endl; 
+	waiting_processes = thread_num;// number of processes that are waiting for a file to process.
 	vector<input_struct*>inputs;
+
+   	// Create threads.
 	for (int i = 0; i < thread_num; i++)
 	{
 		pthread_t tid;
@@ -167,50 +186,27 @@ int main(int argc, char* argv[]) {
 		inputs.push_back(new input_struct);
 		inputs[i]->threadID = i;
 		inputs[i]->query = query;
-		cout << "calling thread " << char(i+65) << endl; 
 		pthread_create(&threads[i],NULL,runner,inputs[i]);
 	}
-	// int counter = 0;
-	// for (int i = 0; i < thread_num; i++)
-	// // vector of abstracts
-	// {
-	// 	// if( i >= thread_num){/* all threads have jobs*/
-	// 	// 	//wait for the next thread then create a new job for it
-	// 	// 	pthread_join(threads[i%thread_num], NULL);//!!!!!!!!!!!!!!!!!!!!!! names are wrong
-	// 	// 	//continue;
-	// 	// }
-	// 	//struct input_struct args;
-	// 	logs.push_back(new input_struct);
-	// 	//logs[i]->input_file_name = file_names[i];
-
-	// 	logs[i]->file_names = &file_names;
-
-	// 	logs[i]->output_file_name = outfile_name;
-	// 	logs[i]->threadID = i;
-	// 	logs[i]->query = query;
-	// 	cout << "calling thread " << logs[i]->threadID << endl; 
-	// 	pthread_create(&threads[i%thread_num],NULL,runner,logs[i]);
-	// }
+	//	Wait for each thread to join back to main.
 	for (int i = 0; i < thread_num; i++)
 		pthread_join(threads[i], NULL);
 	
-	// for (int i = 0; i < num_abstr_to_scan; i++)
-	// {
-	// 	cout << logs[i]->input_file_name <<"---thread " << char(logs[i]->threadID+logs[i]->threadName) << endl;
-	// }
+	//	Sort the logs w.r.t similarity scores.
 	sort(logs.begin(), logs.end(), compareByLength);
 
+	//	Print out the results to the output file.
+	outfile << "###" << endl;
 	for (int i = 0; i < num_abstr_to_return; i++)
 	{
-		outfile << "###" << endl;
 		outfile << "Result "<< i+1 << ":" << endl;
 		outfile << "File: " << logs[i]->abstract_name << endl;
-		//fprintf(outfile,"File: %.4f",logs[i]->similarity);
 		outfile << setprecision(4) << fixed << "Score: " << logs[i]->similarity << endl;
 		outfile << "Summary: " << logs[i]->summary << endl;	
+		outfile << "###" << endl;
 	}
 
-   	printf("Main thread exiting\n");
+	//	Cleaning up.
 	for (int i = 0; i < logs.size(); i++)
 	{
 		delete logs[i];
@@ -223,198 +219,137 @@ int main(int argc, char* argv[]) {
 	outfile.close();
 }
 
-/* The threads will execute in this function */
-void *runner(void *arguments) {
-	// while calculating jaccard similarity save the best sentence to return
-	//string file_name = *((string*) outfile_name);
+void *runner(void *arguments) 
+{
 	struct input_struct *args = (struct input_struct *)arguments;
 	do
 	{
-	cout << "thread " << char(65 + args->threadID) << " working************" << endl;
-		/* */
-		// if(file_name_index == logs.size()){
-		// 	cout << "thread exiting "  << char(65 + args->threadID) << endl;
-		// 	//return output;
-		// 	pthread_exit(NULL);
-		// }
-		/*	request file list access, get a file to process*/
-		pthread_mutex_lock(&file_index_mutex);// file acquired
-		cout << "================================" << file_name_index << endl;
-		if(file_name_index==logs.size()){
-			args->work = false;
+		/*	Request file list access and get a file to process. */
+		pthread_mutex_lock(&file_index_mutex);
+
+		if(file_name_index==logs.size())//Every existing file is acquired, exit the thread.
+		{
+			args->working = false;
 			waiting_processes--;
-			cout << "thread exiting "  << char(65 + args->threadID) <<  " Lastttttttttttttt"<<  endl;
 			pthread_mutex_unlock(&file_index_mutex);
     		pthread_exit(NULL);		
 		}
-		else{
+		else//	There are still some files to be processed.
+		{
+			/*	Acquire a file with the current index. */ 
 			args->input_file_name = logs[file_name_index]->abstract_name;
 			args->current_file_index = file_name_index;
-			//cout << "thread " << char(65 + args->threadID) << " acquired " << args->input_file_name << " --------------"<< endl;
+			//	Increase the index for the next thread.
 			file_name_index++;
+			//	Write to output, informing that this thread is processing the taken file.
 			ofstream outfile;
 			outfile.open(outfile_name, std::ios::app);
 			outfile << "Thread " << char(65 + args->threadID) << " is calculating " << args->input_file_name << endl;
 			outfile.close();
-
-			//pthread_mutex_unlock(&wait_mutex);
-			waiting_processes--;
-			//pthread_mutex_unlock(&wait_mutex);
+			//--
+			waiting_processes--;//	Decrease the number of waiting processes since this thread acquired a file.
 		}
-			pthread_mutex_unlock(&file_index_mutex);// file acquired
 
-		/*Starting calculation*/
+		pthread_mutex_unlock(&file_index_mutex);//	File acquired, will be processed.
+
+		/*	Starting calculation. */
 		jaccard(args);
 
-		/*request file list access to log results*/
+		/*	Request file list access to log the results. */
 		pthread_mutex_lock(&file_list_mutex);
 		logs[args->current_file_index]->similarity = args->jaccard;
 		logs[args->current_file_index]->summary = args->summary;
 		pthread_mutex_unlock(&file_list_mutex);
 
-		// while (waiting_processes>0)
-		// {
-		// 	/* code */
-		// }
-		while(waiting_processes > 0){
-			//cout << waiting_processes << endl;
-		}
-
-		pthread_mutex_lock(&file_index_mutex);
+		while(waiting_processes > 0){}//The thread will not proceed to get a new file if there already is a thread waiting.
+		
+		pthread_mutex_lock(&file_index_mutex);//Checking the exit conditions
 		if(file_name_index == logs.size())	
-			args->work = false;
+			args->working = false;
 		else
 			waiting_processes++;
 		pthread_mutex_unlock(&file_index_mutex);
 		
-	} while(args->work);
-		
-	cout << "thread exiting "  << char(65 + args->threadID) << endl;
-	//return output;
+	} while(args->working);
+	
     pthread_exit(NULL);
 }
 
 void jaccard(input_struct *inputArgs)
 {
+	//	Reading the given input file.
 	ifstream infile;
 	string file_path = "../abstracts/" + inputArgs->input_file_name;
-	//cout << file_path  << "---"<< endl;
 	infile.open(file_path);
 	string abstract;
 	string line;
 	while(getline(infile, line)){
 		abstract += line;
 	}
+
 	string query = inputArgs->query;
-	//cout << inputArgs->input_file_name << "abstract" << abstract << endl;
 	vector<string> abstract_vector;
 	vector <string> query_vector;
-	vector<pair< vector<string>,int> > sentences;
-	//get query set
+	
+	//	Get query set.
 	tokenize(query,&query_vector,' ');
 	set<string> query_set(query_vector.begin(), query_vector.end());
-	//get sentences
+	//	Get sentences.
 	tokenize(abstract,&abstract_vector,'.');
 
+	//	Search the file to find if it includes the query words.
 	set<string> intersection_set ;
 	int best_match = 0;
-	string best_sentence="";
-	for (int i = 0; i < abstract_vector.size(); ++i)
-	{
-		vector<string> temp;
-		tokenize(abstract_vector[i],&temp,' ');
-		//cout << abstract_vector[i] << endl;
-		set<string> temp_set(temp.begin(), temp.end());
-		int matching_words = 0;
-		//*****matching_words = similarity(query_set,temp_set);//number of intersecting words
-		matching_words = similarity(query_set,temp_set,&intersection_set);//number of intersecting words
-		//cout <<"--------" <<matching_words << "********" << endl;
-		if(matching_words > best_match){//finding the best sentence
-			best_match = matching_words;
-			//best_sentence = abstract_vector[i];
-			inputArgs->summary = abstract_vector[i];
-		}
-		sentences.push_back(make_pair(temp,matching_words));
-	}
-	//put all sentences in a set
+	string best_sentence="";//	Summary sentence.
 	set<string> union_set = query_set;
-	int total = 0;
-	for (int i = 0; i < sentences.size(); ++i){
-		for(auto w : sentences[i].first){
-			union_set.insert(w);
+	for (int i = 0; i < abstract_vector.size(); ++i)//Traverse each sentence
+	{
+		//	Tokenize each sentence to seperate words.
+		vector<string> sentence;
+		tokenize(abstract_vector[i],&sentence,' ');
+		set<string> sentence_set(sentence.begin(), sentence.end());
+		int matching_words = 0;
+		matching_words = similarity(query_set,sentence_set,&intersection_set);//	Calculate the number of intersecting words.
+		//	Check if the current sentence is the best up until here.
+		if(matching_words > best_match){
+			best_match = matching_words;
+			inputArgs->summary = abstract_vector[i] + ".";// Update the summary.
 		}
-		//cout << sentences[i].second<< endl;
-		total += sentences[i].second;// !!!!!!!!!!!!!!!!!!!!! same words in different sentences
+		for(auto w : sentence){
+			union_set.insert(w);//	Insert the words in the current sentence to the total union.
+		}
 	}
-	//union_set.insert(query_set.begin(), query_set.end());
 	union_set.insert(".");
-	//cout << inputArgs->input_file_name << "---total-----"<< total << "----------------" << endl;
-	//cout << union_set.size() << "union--------" << endl;
-	double jaccard = double(total) / union_set.size();
-	//cout <<  inputArgs->input_file_name <<"jaccard: " << jaccard << endl;
+	double jaccard = double(intersection_set.size()) / union_set.size();
 	inputArgs->jaccard = jaccard;
-
 }
 
 void tokenize(string inputStr, vector<string>* tokens, char tokenSeperator)
 {
-	// Vector of string to save tokens
-    //vector <string> sentences;
-     
-    // stringstream class check1
     stringstream check1(inputStr);
-     
     string token;
-    //cout <<	inputStr << endl;
-    // Tokenizing w.r.t. space ' '
+    // Tokenizing w.r.t. given seperator.
     while(getline(check1, token, tokenSeperator))
     {
     	string sstemp = token;	
 		stringstream ss(sstemp);
     	string word1;
    		ss >> word1;
-   		//cout << word1 << endl;
-   		if(!word1.empty()){
-    	//cout << token <<  endl;
-        tokens->push_back(token);
-   		}
+   		if(!word1.empty())
+        	tokens->push_back(token);
     }
-    //cout << inputStr << endl;
-    // for(int i = 0; i < tokens.size(); i++)
-    //     cout << tokens[i] << '\n';
 }
 
-int similarity(set<string> query_set, set<string> abstract_set, set<string> *intersection_set){
+int similarity(set<string> query_set, set<string> sentence_set, set<string> *intersection_set){
 
-	int intersectionOld = intersection_set->size();
-	//auto word;
+	int intersectionOld = intersection_set->size();//	Number of intersecting words before current sentence
+	//	Traverse the current sentence and find the intersecting words.
 	for (auto word : query_set)
 	{
-		if(abstract_set.count(word)){
-			//intersection++;
+		if(sentence_set.count(word)){
 			intersection_set->insert(word);
 		}
 	}
-	//cout << "intersection " << intersection << endl;
-	//set<string> union_set = abstract_set;
-	//union_set.insert(query_set.begin(), query_set.end());
-	//cout << "union" << union_set.size() << endl;
+	//	Return the number of intersecting words by looking at how much the intersection set size increased.
 	return intersection_set->size()-intersectionOld;
 }
-// int similarity(set<string> query_set, set<string> abstract_set){
-
-// 	int intersection = 0;
-// 	//auto word;
-// 	for (auto word : query_set)
-// 	{
-// 		if(abstract_set.count(word)){
-// 			intersection++;
-			
-// 		}
-// 	}
-// 	//cout << "intersection " << intersection << endl;
-// 	set<string> union_set = abstract_set;
-// 	union_set.insert(query_set.begin(), query_set.end());
-// 	//cout << "union" << union_set.size() << endl;
-// 	return intersection;
-// }
